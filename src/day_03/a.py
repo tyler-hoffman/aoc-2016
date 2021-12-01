22ef58cf6ec08763e7468c21cf54bdea9e208f55
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-01 14:10:10 -0500

    Move parsing into a

--- src/day_03/a.py
+++ src/day_03/a.py
@@ -1,4 +1,18 @@
-from src.day_03.shared import parse
+import re
+from typing import List
+from src.day_03.shared import Triangle
+
+
+def parse_triangle(line: str) -> Triangle:
+    match = re.search("(\d+)\s+(\d+)\s+(\d+)", line)
+    assert match is not None
+
+    return Triangle(a=int(match.group(1)), b=int(match.group(2)), c=int(match.group(3)))
+
+
+def parse(input: str) -> List[Triangle]:
+    lines = [line for line in input.splitlines() if line]
+    return [parse_triangle(line) for line in lines]
 
 
 def solve(input: str) -> int:
--- src/day_03/shared.py
+++ src/day_03/shared.py
@@ -1,6 +1,4 @@
 import dataclasses
-import re
-from typing import List
 
 
 @dataclasses.dataclass
@@ -14,15 +12,3 @@ class Triangle(object):
         half_length = sum([self.a, self.b, self.c]) / 2
 
         return all([x < half_length for x in [self.a, self.b, self.c]])
-
-
-def parse_triangle(line: str) -> Triangle:
-    match = re.search("(\d+)\s+(\d+)\s+(\d+)", line)
-    assert match is not None
-
-    return Triangle(a=int(match.group(1)), b=int(match.group(2)), c=int(match.group(3)))
-
-
-def parse(input: str) -> List[Triangle]:
-    lines = [line for line in input.splitlines() if line]
-    return [parse_triangle(line) for line in lines]
