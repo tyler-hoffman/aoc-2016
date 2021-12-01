109f61ecc4192853e4f3c3dc92c64c8403334690
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-01 15:07:19 -0500

    Add frequency map

---
+++ test/test_shared/test_functions.py
@@ -0,0 +1,15 @@
+
+import unittest
+
+from src.shared.functions import frequency_map
+
+
+class TestShared(unittest.TestCase):
+    def test_frequency_map(self):
+        input = ["a", "a", "a", "b", "c", "c"]
+        mapped = frequency_map(input)
+        self.assertEqual(mapped, {
+            "a": 3,
+            "b": 1,
+            "c": 2,
+        })
