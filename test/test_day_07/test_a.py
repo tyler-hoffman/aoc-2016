8b46548c4bdfff904dbddf78843d5e5cb90c0592
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-04 12:51:32 -0500

    Solve 07.b

--- test/test_day_07/test_a.py
+++ test/test_day_07/test_a.py
@@ -10,6 +10,7 @@ aaaa[qwer]tyui
 ioxxoj[asdfgh]zxcvbn
 """
 
+
 class TestDay07A(unittest.TestCase):
     def test_solve(self):
         self.assertEqual(solve(SAMPLE_INPUT), 2)
--- test/test_day_07/test_shared.py
+++ test/test_day_07/test_shared.py
@@ -5,13 +5,28 @@ from src.day_07.shared import IpAddress
 
 
 class TestDay07IpAddress(unittest.TestCase):
-    @parameterized.expand([
-        ("abba[mnop]qrst", True),
-        ("abcd[bddb]xyyx", False),
-        ("aaaa[qwer]tyui", False),
-        ("ioxxoj[asdfgh]zxcvbn", True),
-    ])
+    @parameterized.expand(
+        [
+            ("abba[mnop]qrst", True),
+            ("abcd[bddb]xyyx", False),
+            ("aaaa[qwer]tyui", False),
+            ("ioxxoj[asdfgh]zxcvbn", True),
+        ]
+    )
     def test_supports_tls(self, value: str, expected):
         ip_address = IpAddress(value)
 
         self.assertEqual(ip_address.supports_tls, expected)
+
+    @parameterized.expand(
+        [
+            ("aba[bab]xyz", True),
+            ("xyx[xyx]xyx", False),
+            ("aaa[kek]eke", True),
+            ("zazbz[bzb]cd", True),
+        ]
+    )
+    def test_supports_ssl(self, value: str, expected):
+        ip_address = IpAddress(value)
+
+        self.assertEqual(ip_address.supports_ssl, expected)
