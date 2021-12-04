ff09e8c6ea836552220723270441faab4b7b6d68
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-03 22:29:59 -0500

    Solve 06.b

---
+++ test/test_day_06/test_b.py
@@ -0,0 +1,27 @@
+import unittest
+
+from src.day_06.b import solve
+
+
+SAMPLE_DATA = """
+eedadn
+drvtee
+eandsr
+raavrd
+atevrs
+tsrnev
+sdttsa
+rasrtv
+nssdts
+ntnada
+svetve
+tesnvt
+vntsnd
+vrdear
+dvrsen
+enarar
+"""
+
+class TestDay06B(unittest.TestCase):
+    def test_solve(self):
+        self.assertEqual(solve(SAMPLE_DATA), "advent")
