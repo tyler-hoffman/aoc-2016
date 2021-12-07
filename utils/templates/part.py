c79e31bc7e98cab86b40866e6ae200fa9bc0a16b
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-07 18:11:40 -0500

    Update setup scripts

--- utils/templates/part.py
+++ utils/templates/part.py
@@ -2,13 +2,15 @@ PART_TEMPLATE = """
 from src.day_{day_string}.parser import Parser
 from src.day_{day_string}.solver import Solver
 
+class Day{day_string}Part{part_upper}Solver(Solver):
+    @property
+    def solution(self) -> int:
+        return -1
 
-class Day{day_string}Part{part}Solver(Solver):
-    pass
 
 def solve(input: str) -> int:
     parser = Parser()
-    solver = Day{day_string}Part{part}Solver(parser.parse(input))
+    solver = Day{day_string}Part{part_upper}Solver(parser.parse(input))
 
     return solver.solution
 
