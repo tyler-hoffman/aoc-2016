daf3eb8dec81eb15853356deaadfa2ae6d8e9a26
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-20 22:26:21 -0500

    Solve 20.b

--- src/day_20/b.py
+++ src/day_20/b.py
@@ -1,23 +1,30 @@
+from dataclasses import dataclass, field
 from src.day_20.parser import Parser
 from src.day_20.solver import Solver
 
 
-class Day20PartASolver(Solver):
+@dataclass
+class Day20PartBSolver(Solver):
+    tippy_top: int
+
     @property
     def solution(self) -> int:
         max_blocked = -1
 
+        count = 0
         for ip in self.ordered_ips:
-            if ip.start > max_blocked + 1:
-                return max_blocked + 1
+            if ip.start > max_blocked:
+                count += ip.start - max_blocked - 1
+                max_blocked = ip.end
             else:
                 max_blocked = max([max_blocked, ip.end])
-        return max_blocked + 1
+        count += self.tippy_top - max_blocked
+        return count
 
 
-def solve(input: str) -> int:
+def solve(input: str, tippy_top: int = 4294967295) -> int:
     ips = Parser.parse(input)
-    solver = Day20PartASolver(ips=ips)
+    solver = Day20PartBSolver(ips=ips, tippy_top=tippy_top)
 
     return solver.solution
 
--- src/day_20/models.py
+++ src/day_20/models.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from functools import total_ordering
 
+
 @total_ordering
 @dataclass(frozen=True)
 class Range(object):
