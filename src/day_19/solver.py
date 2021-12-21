0db20f51c5ca47280b6d07d8486c86bd562b3946
Tyler Hoffman <tyler.c.hoffman@gmail.com>
2021-12-20 21:32:24 -0500

    Refactory

--- src/day_19/solver.py
+++ src/day_19/solver.py
@@ -1,15 +1,25 @@
 from __future__ import annotations
-from abc import ABC, abstractmethod
+from abc import ABC
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Optional
 
 
 @dataclass
 class Solver(ABC):
-    @property
-    @abstractmethod
-    def solution(self) -> int:
-        ...
+    elf_count: int
+
+    @cached_property
+    def circularly_linked_list(self) -> Link:
+        first_link = Link(value=0)
+        current_link = first_link
+        for i in range(1, self.elf_count):
+            new_link = Link(value=i)
+            current_link.next = new_link
+            current_link = new_link
+        current_link.next = first_link
+        return first_link
+
 
 @dataclass
 class Link(object):
